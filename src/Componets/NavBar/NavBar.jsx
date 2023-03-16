684ab7ef213167faa10b9a0e98131b3a3f036ea2
chigoox <dikeemmanuel54@gmail.com>
2023-03-16 04:30:43 -0400

    updated

--- src/Componets/NavBar/NavBar.jsx
+++ src/Componets/NavBar/NavBar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { BsSearch, BsPerson, BsCart2, BsFillMenuButtonWideFill } from "react-icons/bs";
 
 function NavBar() {
     const navItems = ['Home', 'Shop', 'Product', 'Blog', 'Pages']
-    const NavItemsLook = ({ item }) => { return (<h1 className='font-bold text-xs'>{item}</h1>) }
+    const NavItemsLook = ({ item }) => { return (<h1 className='font-bold text-xs trans hover:scale-105 text-slate-400 hover:text-white hover:underline  underline-offset-8'>{item}</h1>) }
     return (
         <div className='flex items-center p-12 h-[76px] w-full fixed z-10'>
-            <div className="w-42 md:w-64 h-10 mr-16 font-bold text-sm md:text-lg">Humaine Grandure</div>
-            <div className='w-[550px] p-2 scale-0 md:scale-100 flex trans-slow justify-around items-center text-white'>
+            <div className="w-32 lg:w-64 h-10 font-bold text-sm md:text-lg">Humaine Grandure</div>
+            <div className='w-[550px] p-2 scale-0 md:scale-90 lg:scale-100 flex trans-slow justify-around items-center text-white'>
                 {navItems.map((item) => { return <NavItemsLook item={item} /> })}
             </div>
-            <div className="flex-grow h-12 scale-0 md:scale-100 w-96 trans-slow">
-                <div className="flex justify-end items-center">
-                    <div className='h-6 w-6 bg-white m-2'></div>
-                    <div className='h-6 w-6 bg-white m-2'></div>
+            <div className="flex-grow h-12 scale-0 md:scale-90 lg:scale-100 w-96 trans-slow">
+                <div className="flex justify-end items-center text-xl">
+                    <button className='h-6 w-6 m-2'><BsSearch /></button>
+                    <button className='h-6 w-6 m-2'><BsPerson /></button>
                     <div className='flex items-center text-sm'>
-                        <div className='h-6 w-6 bg-white m-2'></div>
+                        <button className='h-6 w-6  m-2 text-xl'><BsCart2 /></button>
                         <h1>$9999</h1>
                     </div>
-                    <div className='h-12 border mx-4'></div>
-                    <div className='h-6 w-6 bg-white m-2'></div>
+                    <div className='h-12 border mx-4 '></div>
+                    <button className='h-6 w-6 m-2'><BsFillMenuButtonWideFill /></button>
                 </div>
             </div>
         </div>
--- src/Pages/ShopPage.jsx
+++ src/Pages/ShopPage.jsx
@@ -10,6 +10,25 @@ function ShopPage() {
 
 
             </div>
+            <div className="w-[50%]">
+                <div className="items-center justify-center w-48 md:w-96 m-auto h-96 p-4 top-24  relative">
+                    <div className='text-sm'>
+                        <p>Autumn</p>
+                        <p>Collection</p>
+                        <p>2023</p>
+                    </div>
+                    <div className='my-4'>
+                        <h1 className='text-5xl md:text-7xl'>Autum</h1>
+                        <h1 className='text-5xl md:text-7xl'>Vibes</h1>
+                    </div>
+                    <div className=''>
+                        <p>Casual line. short desgin. 100% suade lather. etc that make store great</p>
+                    </div>
+
+                    <button className='my-12 border-2 p-2 border-gray-500 trans-slow hover:text-black hover:bg-gray-300'>Shop Now</button>
+                </div>
+
+            </div>
         </div>
     )
 }
