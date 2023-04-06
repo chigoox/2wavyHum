e600aab38fad5c2eed49686195fcdcf4236667a0
chigoox <dikeemmanuel54@gmail.com>
2023-04-06 03:47:57 -0400

    update

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <div className=' w-full '>
-            <div className='bg-[#141414] text-xs gap-12 h-fit  p-14 flex flex-col md:flex-row justify-start items-start'>
-                <div className='p-2'>
+            <div className='bg-[#141414] fadeInBottom text-xs gap-12 h-fit  p-14 flex flex-col md:flex-row justify-start items-start'>
+                <div className='p-2 fadeInRight'>
                     <h1 className='my-1 font-bold text-2xl'>Humaine Grandeur</h1>
                     <h1 className='my-1 '>will add adress here if so?</h1>
                     <h1 className='my-1 '>+1234567890</h1>
@@ -17,7 +17,7 @@ function Footer() {
                         <button className="h-4 w-4 trans hover:scale-105 bg-white"></button>
                     </div>
                 </div>
-                <div className='my-10 p-2 h-full w-fit'>
+                <div className='my-10 p-2 h-full w-fit fadeInRight'>
                     <h1 className='text-2xl '> Company</h1>
                     <div className='h-12 '>
                         <Link to={'/About'} className='block'>Contact</Link>
@@ -25,7 +25,7 @@ function Footer() {
                     </div>
                 </div>
 
-                <div className=' h-fit'>
+                <div className=' h-fit fadeInRight'>
                     <div className='p-2 h-fit'>
                         <h1 className='m-1 font-bold text-2xl'>Newletter</h1>
                         <h1 className='m-1 '>Be the first to hear
