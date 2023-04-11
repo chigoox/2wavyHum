dade1c3079c55800baca6f5cf543c4c425ef010d
chigoox <dikeemmanuel54@gmail.com>
2023-04-11 16:41:02 -0400

    pix

--- src/Componets/Shop/ItemIcon.jsx
+++ src/Componets/Shop/ItemIcon.jsx
@@ -14,14 +14,14 @@ function ItemIcon({ priceID, img, price, name, rating, salePrice, desc, togglePr
 
 
     return (
-        <button disabled={cartHovered} onMouseOver={setItem} onClick={openProductWindow} className={`group cursor-pointer h-[382px] fadeInBottom m-auto scale-125 md:scale-100 w-[260px] md:hover:scale-110 hover:scale-[1.3] trans  mt-16 md:mt-5 relative mb-12 overflow-hidden z-0 rounded-xl`}>
+        <button disabled={cartHovered} onMouseOver={setItem} onClick={openProductWindow} className={`group cursor-pointer h-[400px] md:h-[382px] fadeInBottom m-auto scale-125 md:scale-100 w-[260px] md:hover:scale-110 hover:scale-[1.3] trans  mt-16 md:mt-5 relative mb-12 overflow-hidden z-0 rounded-xl`}>
             {salePrice && <div className='absolute top-0 left-0 m-2 h-6 w-10 z-10 bg-gray-900 rounded-lg items-center justify-center flex'>Sale</div>}
             <div className='md:group-hover:bottom-16 bottom-0 relative trans flex flex-col gap-5 '>
                 <div className='bg-white w-[260px] h-[260px]'>
                     <img src={img} className={'w-full h-full object-cover'} alt="" />
 
                 </div>
-                <div className='h-[83px] flex flex-col gap-2 relative bottom-10 md:bottom-0'>
+                <div className='h-[83px] flex flex-col gap-2 '>
                     <h1 className=' font-bold text-xl w-fit'>{name}</h1>
                     <Rating rate={rating} />
                     <div className="flex gap-2">
