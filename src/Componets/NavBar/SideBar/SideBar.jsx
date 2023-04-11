329dccc1934e13a1d094d09a7263cdda9655dacf
chigoox <dikeemmanuel54@gmail.com>
2023-04-11 17:47:37 -0400

    asd

--- src/Componets/NavBar/SideBar/SideBar.jsx
+++ src/Componets/NavBar/SideBar/SideBar.jsx
@@ -61,8 +61,8 @@ function SideBar({ clientCart, setClientCart, cartTotal }) {
             />
             <div className={`w-full items-center flex flex-col justify-center  bg-[#070707] shadow-sm shadow-black h-full transition-all duration-500 ease-in-out`}>
                 <div className="w-full h-20">
-                    <div className='center text-sm z-10'>
-                        <button onClick={toggleCart} className='h-6 w-6  hover:scale-110  m-2 text-4xl trans-slow center'><BsCart2 />{(cartTotalSum > 0) && <h1 className="text-base">${cartTotalSum}</h1>}</button>
+                    <div className='center text-sm z-10 h-full'>
+                        <button onClick={toggleCart} className='h-6 w-6  hover:scale-110  m-2 text-3xl trans-slow center'><BsCart2 />{(cartTotalSum > 0) && <h1 className="text-base">${cartTotalSum}</h1>}</button>
 
                     </div>
                 </div>
