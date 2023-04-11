a50f1f494b10e5a4fa9651a1ff4ebe33891dc02a
chigoox <dikeemmanuel54@gmail.com>
2023-04-11 17:58:02 -0400

    undefined redirects to shop from its own page

---
+++ src/Pages/UndefinedP.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function UndefinedP() {
+
+
+
+    setTimeout(() => {
+        window.location.href = '/Shop'
+
+    }, 3000)
+
+
+    return (
+        <div className={`h-full w-full flex-col flex fadeInZoom items-center text-8xl`} >
+            by ED5...
+
+
+
+        </div>
+    )
+}
+
+export default UndefinedP
